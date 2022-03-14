9edbf39710974d583af1b0324b1614bea5a24af5
Thomas Compton IV <tacompton4@gmail.com>
2022-03-13 23:01:32 -0400

    start of color picker

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -4,11 +4,15 @@ import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 
 
 class MainActivity : AppCompatActivity() {
 
     private var drawingView: DrawingView? = null
+    private var imageButtonCurrentColor : ImageButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,10 +21,17 @@ class MainActivity : AppCompatActivity() {
         drawingView = findViewById(R.id.drawing_view)
         drawingView?.setSizeForBrush(15.toFloat())
 
+        val linearLayoutColors = findViewById<LinearLayout>(R.id.ll_paint_colors)
+
         val brushIcon: ImageButton = findViewById(R.id.ib_brush)
         brushIcon.setOnClickListener {
             showBrushSizeDialog()
         }
+
+        imageButtonCurrentColor = linearLayoutColors[1] as ImageButton
+        imageButtonCurrentColor!!.setImageDrawable(
+            ContextCompat.getDrawable(this, R.drawable.pallet_selected)
+        )
     }
 
     private fun showBrushSizeDialog() {
